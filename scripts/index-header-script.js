4b5e568bf61f0c12b41760a34067b2bbf61764e5
desdotjs <desdotjs@gmail.com>
2026-01-30 14:47:20 -0600

    updates

---
+++ scripts/index-header-script.js
@@ -0,0 +1,15 @@
+
+const headerArt = [
+
+]
+
+let storedIndex = localStorage.getItem("headerArt");
+
+let index = storedIndex === null ? 0: Number(storedIndex);
+
+index++;
+
+if (index >= headerArt.length) index = 0;
+
+localStorage.setItem("headerArt", index);
+

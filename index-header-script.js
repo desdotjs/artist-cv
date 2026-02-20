bc6a808a7e7d7c371110ac6f49dcc7e603865544
desdotjs <desdotjs@gmail.com>
2026-02-20 15:07:49 -0600

    pissin

--- index-header-script.js
+++ index-header-script.js
@@ -17,3 +17,5 @@ if (index >= headerArt.length) index = 0;
 
 localStorage.setItem("headerArt", index);
 
+
+
--- w31rdgr1wr1d-pages/w31rdgr1wr1d-scripts/cowie.js
+++ w31rdgr1wr1d-pages/w31rdgr1wr1d-scripts/cowie.js
@@ -1,53 +1,4 @@
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-
-// CALLING BUILT IN CANVAS API
-
-// const canvas = document.getElementById("canvas");
-// const ctx = canvas.getContext("2d");
-// getContext is a method that belongs to canvas
-// getElementID is a method that belongs to document - "hey browser, get me the element with the id of "canvas" (technically you could name it poopfart and it would still work)
-
-// canvas.width  = window.innerWidth // window is a global object built-in the browser
-// canvas.height = window.innerHeight // property that belongs to window
-
-// STORING IMAGES AS THEIR OWN OBJECTS
-
-// const cowieImg = new Image();
-
-// cowieImg.src = 'w31rdgr1wr1d-static/cowie.png';
-
-// cowieImg.onload = () => {
-// anonymous function is a function without a variable function such as onload
-//   draw();
-//   console.log("cowie is ready! ^_^")
-
-// };
-
-// const textboxImg = new Image();
-
-// textboxImg.src = 'w31rdgr1wr1d-static/textbox.png';
-
-// textboxImg.onload = () => {
-  
-//   draw();
-//   console.log("cowie has thoughts to share!")
-
-// };
-
-// textboxImg.onload = () => { // empty parathesis = "this function has no parameters" (since... onload has no)
-
-//     console.log("cowie has thoughts to share!")
-
-// }
-
-// variable.property = (parameters) => { body }
-
-
-// onload is an event-handling property on the image object
-// . means access a property or method that belongs to this thing
-
-
 // AUDIO FILES OBJECT STORAGE
 
 const soundFiles = {
@@ -83,19 +34,8 @@ const soundFiles = {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-// document is a built-in object in browsers that represents the webpage.
-// addEventListener is a method that listens for a specific event (in this case, 'DOMContentLoaded') and runs a function when that event occurs. The function is defined as an arrow function () => { ... } which will execute when the DOM content is fully loaded.
-// typically unecessary if script is going to live in <body> but good practice to ensure script doesnt run before page is loaded
-
-    console.log('page loaded! ^_^');
 
 });
-
-// parenthesis is for a single expression only. "=>" can only read one thing. () => (expression)
-// brackets are for a statement
-// eventListeners almost always need a bracket
-
-// KEYBOARD EVENT LISTENER
 
 document.addEventListener('keydown', (event) => {
   
@@ -107,17 +47,3 @@ document.addEventListener('keydown', (event) => {
   }
 
 })
-
-// function draw() { 
-
-  // drawImage built-in canvas api method that draws an image on the canvas
-  // takes in parameters of (image, x, y)
-  // draw function, just like p5.js!
-
-//   ctx.drawImage(cowieImg, 100, 100);
-//   ctx.drawImage(textboxImg, 200, 200);
-
-
-// }
-
-// jake lamb said ditch canvas :-(
